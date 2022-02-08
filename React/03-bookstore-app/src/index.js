551dd5cab2c669b71f105836ee0b7f52d35887d5
Klejdi Jatagani <famkowalsky@gmail.com>
2022-02-08 21:13:44 +0100

    Add gitignore

--- React/03-bookstore-app/src/index.js
+++ React/03-bookstore-app/src/index.js
@@ -1,14 +1,13 @@
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import store from "./store/index";
-import "./index.css";
-import App from "./App";
-
+import store from './store/index';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- React/04-github-app/src/App.js
+++ React/04-github-app/src/App.js
@@ -1,18 +1,20 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from 'react-router-dom';
 
-import NotFound from "./pages/NotFound";
-import AllUsers from "./pages/AllUsers";
-import UserDetail from "./pages/UserDetail";
-import Layout from "./components/Layout/Layout";
+import NotFound from './pages/NotFound';
+import AllUsers from './pages/AllUsers';
+import UserDetail from './pages/UserDetail';
+import Layout from './components/Layout/Layout';
+
+// Comment:
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/users" />} />
-        <Route path="/users" element={<AllUsers />} />
-        <Route path="/users/:userId" element={<UserDetail />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path='/' element={<Navigate replace to='/users' />} />
+        <Route path='/users' element={<AllUsers />} />
+        <Route path='/users/:userId' element={<UserDetail />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Layout>
   );
