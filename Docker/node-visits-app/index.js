99c463f58696d1079412c84c65422297ad2dfd64
Klejdi Jatagani <famkowalsky@gmail.com>
2022-07-12 10:13:49 +0200

    Setup with docker-compose

---
+++ Docker/node-visits-app/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createClient } = require('redis');
+
+const app = express();
+
+const client = createClient({
+  url: 'redis://redis-server:6379'
+});
+
+(async() => {
+  await client.connect();
+  await client.set('visits', 0);
+})();
+
+client.on('connect', () => console.log('Redis Client Connected...'));
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+app.get('/', async (req, res) => {
+  await client.get('visits', (err, visits) => {
+    res.send('Number of visits: ' + visits);
+    await client.set('visits', parseInt(visits) + 1);
+  });
+});
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081')
+});
